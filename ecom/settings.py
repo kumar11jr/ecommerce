7f145b1a8f37b17376c4dcf436e633e553d78ee1
Prabhat Kumar <104729493+kumar11jr@users.noreply.github.com>
2023-08-03 18:37:23 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shahil11jr@gmail.com'
-EMAIL_HOST_PASSWORD = 'ozsyxvpkmlsvyiri'  # Or use an app-specific password if enabled
+EMAIL_HOST_USER = '#################'
+EMAIL_HOST_PASSWORD = '###############'  # Or use an app-specific password if enabled
