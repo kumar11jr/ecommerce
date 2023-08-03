ca713f7861586d04062551d4b1eb6671aa5c3713
kumar11jr <kumar11jr@gmail.com>
2023-08-03 23:15:42 +0530

    day 2

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from base.models import BaseModel
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -7,6 +8,15 @@ class Category(BaseModel):
     category_name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True,null=True,blank=True)
     category_img = models.ImageField(upload_to="categories")
+
+    def save(self,*args, **kwargs):
+        self.save = slugify(self.category_name)
+        super(Category,self).save(*args, **kwargs)
+
+
+    def __str__(self) -> str:
+        return self.category_name
+
 
     
 class Product(BaseModel):
