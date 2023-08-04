7ddc4fc1fe5208dab537fa514188a2a90266fd58
kumar11jr <kumar11jr@gmail.com>
2023-08-04 20:24:02 +0530

    day 3

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from products.models import Product
 # Create your views here.
 def index(request):
-    return render(request,"home/index.html")
+    context = {'products':Product.objects.all()}
+    return render(request,"home/index.html",context)
