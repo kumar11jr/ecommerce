0259c646a70dd44af307b84b1cc2c1ade8588fb8
kumar11jr <kumar11jr@gmail.com>
2023-08-04 19:44:22 +0530

    day3

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,21 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Product)
+
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+    
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
+    
+    
+admin.site.register(Product,ProductAdmin)
+
+
+admin.site.register(ProductImage)
+
+
+
--- products/models.py
+++ products/models.py
@@ -9,11 +9,10 @@ class Category(BaseModel):
     slug = models.SlugField(unique=True,null=True,blank=True)
     category_img = models.ImageField(upload_to="categories")
 
-    def save(self,*args, **kwargs):
-        self.save = slugify(self.category_name)
-        super(Category,self).save(*args, **kwargs)
-
-
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)  # Assign slugified category_name to slug field
+        super(Category, self).save(*args, **kwargs)
+    
     def __str__(self) -> str:
         return self.category_name
 
@@ -21,10 +20,18 @@ class Category(BaseModel):
     
 class Product(BaseModel):
     product_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True,null=True,blank=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
+    
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.product_name)  # Assign slugified category_name to slug field
+        super(Product, self).save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.product_name
 
     
 class ProductImage(BaseModel):
