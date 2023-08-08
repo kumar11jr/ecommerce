99237e43451fcdba089269d28893fa2049dbb02f
kumar11jr <kumar11jr@gmail.com>
2023-08-08 18:53:24 +0530

    day 3

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from products.models import Product
 def get_product(request,slug):
     try:
         product = Product.objects.get(slug=slug)
-    
+     
         return render(request,"product/product.html",context={'product':product})
     
     except Exception as e:
